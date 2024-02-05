855fb1fe23952fca08dcb818f4fef93dc6d91b15
alexis <atroyvb@gmail.com>
2024-02-05 18:24:30 -0500

    Extra commits

--- week 2 /grants_data.py
+++ week 2 /grants_data.py
@@ -1,25 +1,24 @@
-from re import split
 import pandas as pd
 
 
 class GrantsData:
-
     def __init__(self, path: str):
-        self.df = pd.read_csv(path, compression= 'zip')
+        self.df = pd.read_csv(path, compression='zip')
 
     def read(self) -> pd.DataFrame:
         """Returns a cleaned dataframe"""
         df = self._select_columns(self.df)
-        # data can have NaNs
-        # different types (reasonable)
-        # different types (unreasonable)
-
+        df = self._clean(df)
+        # Data can have NaNs
+        # Different types (reasonable)
+        # Different types (unreasonable)
         print(self.df)
 
-    @staticmethod  #means it doesn't use anything from self
+    @staticmethod
     def _select_columns(df: pd.DataFrame) -> pd.DataFrame:
-        """ Rename and select columns
-        NOTE: underscore methods are "private methods" meaning we should on call them from WITHIN the class
+        """Rename and select columns
+        NOTE: Underscored methods are "private methods", otherwise 
+        meaning that we should only call them from WITHIN the class.
 
         Args:
             df (pd.DataFrame): dataframe
@@ -27,56 +26,79 @@ class GrantsData:
         Returns:
             pd.DataFrame: the subset, clean name dataframe
         """
-        mapper= {
-            'APPLICATION_ID':'application_id',
-            'BUDGET_START': 'budget_start', 
-            'ACTIVITY' : 'grant_type',
-            'TOTAL_COST' : 'total_cost',
-            'PI_NAMEs' : 'pi_names',
-            'PI_IDS' : 'pi_ids', 
-            "ORG_NAME" : 'organization',
-            'ORG_CITY' :'city', 
-            "ORG_STATE" : 'state',
-            'ORG_COUNTRY' : 'country'
+        mapper = {
+            'APPLICATION_ID': 'application_id',
+            'BUDGET_START': 'budget_start',
+            'ACTIVITY': 'grant_type',
+            'TOTAL_COST': 'total_cost',
+            'PI_NAMEs': 'pi_names',
+            'PI_IDS': 'pi_ids',
+            'ORG_NAME': 'organization',
+            'ORG_CITY': 'city',
+            'ORG_STATE': 'state',
+            'ORG_COUNTRY': 'country'
         }
         return df.rename(columns=mapper)[mapper.values()]
+    
+    @staticmethod
+    def _clean(df: pd.DataFrame) -> pd.DataFrame:
+        """Remove NaNs and other cleaning functions
 
-@staticmethod
-def _clean(df: pd.DataFrame) -> pd.DataFrame:
-    """Remove Nans and other cleaning functions
+        Args:
+            df (pd.DataFrame): dataframe with subset column names
 
-    Args:
-        df (pd.DataFrame): dataframe with subset column names 
+        Returns:
+            pd.DataFrame: dataframe free of NaNs
+        """
+        df['pi_names'] = df['pi_names'].str.split(';')
+        df = df.explode('pi_names')
+        df['is_contact'] = df['pi_names'].str.lower().str.contains('(contact)')
+        df['pi_names'] = df['pi_names'].str.replace('(contact)', '')
+        df['both_names'] = df['pi_names'].apply(lambda x: x.split(',')[:2])
+        df[['last_name', 'forename']] = pd.DataFrame(df['both_names'].to_list(), index=df.index)
+        print(df)
 
-    Returns:
-        pd.DataFrame: DF free of NaNs
-    """
-    df['pi_names'] = df['pi_names'].str.split(';')
-    df = df.explode('pi_names')
-    df['is_contact'] = df['pi_names'].str.lower().str.contains('(contact)')
-    df['pi_names'] = df['pi_names'].str.replace('(contact)', '') 
-    df[['both_names']] = df['pi_names'].apply(lambda x: x.split(',')[:2])
-    df[['last_name', 'forename']] = pd.DataFrame(df['both_names'].to_list(), index = df.index)
-    print(df)
+        
 
-#  HW TO IMPUTE THE DATES 
+
 
 def read_grants_year(year: int | str) -> pd.DataFrame:
-    """Reads in Grants Data for a year and return as clean dataframe.
+    """Read in Grants Data for a year and return as clean dataframe
 
     Args:
         year (int | str): year to read
 
     Returns:
-       pd.DataFrame: clean data frame of grants data 
-     """
+        pd.DataFrame: clean dataframe of grants data
+    """
     # We know the filename is: RePORTER_PRJ_C_FY2022.zip
     path = "/Users/alexistroy/Downloads/RePORTER_PRJ_C_FY2022.zip"
     gd = GrantsData(path.format(year=year))
     return gd.read()
 
+
+
 if __name__ == '__main__':
-    import numpy as np 
+    import numpy as np
+    # '/mnt/search/data/grants/RePORTER_PRJ_C_FY2022.zip'
+
+    vec1 = [i for i in range(1_000_000)]
+    vec2 = np.arange(1_000_000)
 
     read_grants_year(2022)
-    #gd = GrantsData()
+    # gd = GrantsData()
+
+def fill_in_budget(year: int | str) -> pd.DataFrame:
+    """ To fill in the missing dates for the 'budget_start' column
+
+    Args:
+        year (int | str): year to read 
+
+    Returns:
+        pd.DataFrame: All of the dates filled in 
+    """
+    path = "/Users/alexistroy/Downloads/RePORTER_PRJ_C_FY2022.zip"
+    gd2 = GrantsData(path.format(year=year))
+    
+    
+    
